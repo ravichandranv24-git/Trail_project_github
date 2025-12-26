b08f4513afa65b33c69f4edc1afc3d8b6bf2172c
ravichandranv24-git <ravichandranv24@gmail.com>
2025-12-26 17:52:12 +0530

    Update cont1.java

--- src/test/java/contacts/cont1.java
+++ src/test/java/contacts/cont1.java
@@ -4,6 +4,9 @@ public class cont1 {
 
 	public static void main(String[] args) {
 		System.out.println("----contacts created-----------");
+		System.out.println("----contacts created and deleted-----------");
+
+
 
 	}
 
